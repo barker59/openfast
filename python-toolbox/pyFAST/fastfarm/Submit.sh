6f92a795e44d78bc77751fbf3a236037678f732a
Aaron Barker <barker59@gmail.com>
2021-09-28 10:10:57 -0600

    Adds eagle run script 'Submit.sh'

---
+++ python-toolbox/pyFAST/fastfarm/Submit.sh
@@ -0,0 +1,24 @@
+#! /bin/bash
+#SBATCH --time 4:00:00
+#SBATCH -A windse
+#SBATCH --nodes=1
+#SBATCH --ntasks-per-node=36
+#SBATCH --job-name I35_C000_S0
+
+module purge
+module load comp-intel mkl
+
+cd Seed_0
+/home/kshaler/OpenFAST_9.30.19/OpenFAST/build/glue-codes/fast-farm/openfast-farm FFarm_mod.fstf > output.txt &
+cd ../Seed_1
+/home/kshaler/OpenFAST_9.30.19/OpenFAST/build/glue-codes/fast-farm/openfast-farm FFarm_mod.fstf > output.txt &
+cd ../Seed_2
+/home/kshaler/OpenFAST_9.30.19/OpenFAST/build/glue-codes/fast-farm/openfast-farm FFarm_mod.fstf > output.txt &
+cd ../Seed_3
+/home/kshaler/OpenFAST_9.30.19/OpenFAST/build/glue-codes/fast-farm/openfast-farm FFarm_mod.fstf > output.txt &
+cd ../Seed_4
+/home/kshaler/OpenFAST_9.30.19/OpenFAST/build/glue-codes/fast-farm/openfast-farm FFarm_mod.fstf > output.txt &
+cd ../Seed_5
+/home/kshaler/OpenFAST_9.30.19/OpenFAST/build/glue-codes/fast-farm/openfast-farm FFarm_mod.fstf > output.txt &
+wait
+
